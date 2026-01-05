6f59d027757a7aa0e106f7758712c7ee1f06a1fe
amitkr79 <amitraj8655977@gmail.com>
2026-01-05 17:33:16 +0530

    configured media files in project settings

---
+++ blogs/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50,unique=True)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    
+    class Meta:
+        verbose_name_plural='categories'
+    
+    def __str__(self):
+        return f"{self.category_name}"
